2d2cbd8d8d131715760339614829377b8f07314e
Isilud <jolan.houot@gmail.com>
2023-08-24 17:23:55 +0200

    Fix Java version & dependency

--- SafetyNetAlert/src/main/java/com/safetynet/SafetyNetAlert/SafetyNetAlertApplication.java
+++ SafetyNetAlert/src/main/java/com/safetynet/SafetyNetAlert/SafetyNetAlertApplication.java
@@ -1,18 +1,13 @@
 package com.safetynet.SafetyNetAlert;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SafetyNetAlertApplication implements CommandLineRunner {
+public class SafetyNetAlertApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafetyNetAlertApplication.class, args);
 	}
 
-	@Override
-    public void run(String... args) throws Exception {
-        System.out.println("Starting !");
-    }
 }
